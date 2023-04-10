dba0c1a0688f40b57e575bfd5a27b00aaaf63718
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-04-10 14:45:16 +0700

    Add project files.

---
+++ class_oop_205/class_oop_205.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+    int nim;
+    string nama;
+    void tampil() {
+        cout << "NIM= " << nim;
+        cout "\nNama= " << nama;
+
+    }
+};
+
+
